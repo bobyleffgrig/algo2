afc6f57a0322a583424dfcb2d02a8def8252c162
bobyleffgrig <91370733+bobyleffgrig@users.noreply.github.com>
2021-11-20 12:49:32 +0300

    Add files via upload

---
+++ craps_rol 1 er.py
@@ -0,0 +1,7 @@
+import time
+import random
+diel  =  random.randint(1,6)
+die2  =  random.randrange(6)  +  1 
+total  =  diel  +  die2 
+print(f"Пpи  вашем броске выпало .  {diel}.  и.  {die2}.  очков.  в  сумме.  {total}") 
+input("\n\nHaжмитe Enter.  чтобы выйти . ") 
